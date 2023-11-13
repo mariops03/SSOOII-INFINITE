77b1b4123ba71adccb8ea4c50496a0064f372bce
Daniel Mulas Fajardo <danimulas28@gmail.com>
2023-11-13 20:12:40 +0100

    Revert "Practica hecha con H2->N2->H1"
    This reverts commit 1ad99a7534b6d59afda62127a24483dba8789ba3.

--- notwork.c
+++ notwork.c
@@ -14,7 +14,7 @@ struct sigaction manejadoraT;
 struct sigaction manejadoraA;
 pid_t pid;
 int contador=0;
-pid_t hijos[2];
+
 void mascarabloqueo(){
 
     sigfillset(&maskPadre); //cargamos la mascara con todas las señales
@@ -36,8 +36,8 @@ void funcionTestigo(){
 
 }
 void set_process_name(const char* name) {
-    //prctl(PR_SET_NAME, name, NULL, NULL, NULL);
-    printf("Bien creado:  %s\n", name);
+    prctl(PR_SET_NAME, name, NULL, NULL, NULL);
+    printf("Nombre del proceso cambiado a %s\n", name);
 }
 
 void process_code(const char* name) {
@@ -56,60 +56,53 @@ void manejadoraTestigo(){
 int main() {
     mascarabloqueo();
     mascaratestigo();
-    manejadoraTestigo();
     sigprocmask(SIG_SETMASK,&maskPadre,NULL);  // activamos la mascara y la hereda el hijo
 
-    int i;
-    for(i=0;i<2;i++){
-        hijos[i]=fork();
-
-        if(hijos[i]==0){
-            //Codigo de los hijos
-            if(i==0){
-                //Codigo de H1
-                set_process_name("H1");
-                sigprocmask(SIG_SETMASK,&maskTestigo,NULL);  
-                //disparo al padre
-                pidDisparo = getppid();
-                while(1){
-                    sigsuspend(&maskTestigo);
-                   // printf("Disparo al padre\n");
-                    kill(pidDisparo,SIGUSR1);
-
-                }
-            }
-            if(i==1){
-               //Codigo de H2
-                set_process_name("H2");
-                sigprocmask(SIG_SETMASK,&maskTestigo,NULL);  
-                pidDisparo = N2;
-                while(1){
-                    sigsuspend(&maskTestigo);
-                    printf("Disparo a N2\n");
-                    kill(pidDisparo,SIGUSR1);
-                    
-                }
+   //EL padre tiene tiene de hijo a H1
+    H1=fork();
+    if(H1 > 0){
+        //COdigo del padre
+        //El padre tiene de hijo a H2
+         H2=fork();
+         
+        //H2 tiene de hijo a N2
+        if(H2 >0){
+            //Codigo de H2
+            set_process_name("H2");
+            sigprocmask(SIG_SETMASK,&maskTestigo,NULL);  
+            pidDisparo = N2;
+            while(1){
+                sigsuspend(&maskTestigo);
+                printf("Disparo a N2\n");
+                kill(pidDisparo,SIGUSR1);
                 
             }
         }
-    }
-         //COdigo del padre
-        //El padre tiene de hijo a H2
-            printf("Soy el padre y mi hijo es H1: %d H2:%d\n",H1, H2);
-         //impriro el pid de H2
-            printf("Soy el padre y mi hijo es %d\n",H2);
-            //imprimo el pid de H1
-            printf("Soy el padre y mi hijo es %d\n",H1);
-            sleep(2);
+        if(H2 == 0){
+            //Codigo de N2
+            set_process_name("N2");
+            pidDisparo = H1;
             while(1){
-                pidDisparo = H2;
-
-               // printf("Soy el padre y disparo a H2 : %d\n",pidDisparo);
-                printf("Circulo %d\n",contador);
-                contador++;
-                kill(pidDisparo,SIGUSR1);
                 sigsuspend(&maskTestigo);
+                printf("Disparo a H1\n");
+                kill(pidDisparo,SIGUSR1);
+                
             }
-}
+        }
 
-  
+
+    }
+    if(H1 == 0){
+        //Codigo de H1
+        set_process_name("H1");
+        sigprocmask(SIG_SETMASK,&maskTestigo,NULL);  
+        //disparo al padre
+        pidDisparo = getppid();
+        while(1){
+            sigsuspend(&maskTestigo);
+            printf("Disparo al padre\n");
+            kill(pidDisparo,SIGUSR1);
+
+        }
+    }
+}
