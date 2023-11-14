9d10a6d045c7134ec5bd49a4c6436c639a6e655e
Daniel Mulas Fajardo <danimulas28@gmail.com>
2023-11-14 14:34:44 +0100

    "TERMINADO"

--- infinito.c
+++ infinito.c
@@ -3,48 +3,47 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/prctl.h>
+//#include <sys/prctl.h>
 #include <string.h>
 
 pid_t H1, H2, N2, H3, N3, H4, H3;
-pid_t pidpadre;
 sigset_t maskPadre;
 sigset_t maskTestigo;
 int pidDisparo;
 struct sigaction manejadoraT;
 struct sigaction manejadoraA;
 struct sigaction terminateHandler;
-pid_t pid;
 int contador=0;
 void mascarabloqueo(){
 
-    sigfillset(&maskPadre); //cargamos la mascara con todas las señales
-    sigdelset(&maskPadre,SIGINT); //quitamos SIGINT
-   // sigprocmask(SIG_SETMASK,&maskPadre,NULL);  // activamos la mascara
+    sigfillset(&maskPadre); 
+    sigdelset(&maskPadre,SIGINT); 
+
 }
-//mascara para pasar el testigo
+
 void mascaratestigo(){
 
-    sigfillset(&maskTestigo); //cargamos la mascara con todas las señales
-    sigdelset(&maskTestigo,SIGUSR1); //quitamos SIGUSR1
-    sigdelset(&maskTestigo,SIGINT); //quitamos SIGINT
-    sigdelset(&maskTestigo,SIGALRM); //quitamos SIGALRM
+    sigfillset(&maskTestigo); 
+    sigdelset(&maskTestigo,SIGUSR1); 
+    sigdelset(&maskTestigo,SIGINT); 
+    sigdelset(&maskTestigo,SIGALRM); 
     sigdelset(&maskTestigo,SIGTERM); 
     sigdelset(&maskTestigo,SIGKILL);
-   // sigprocmask(SIG_SETMASK,&maskTestigo,NULL);  // activamos la mascara
 }
 void funcionTestigo(){
+
     kill(pidDisparo,SIGUSR1);
-    //exit(0);
 
 }
 void funcionAlarma(){
     //Tengo que matar a todos los procesos
     printf("ALARMA, time DONE\n");
-    exit(0);
-    //kill(H3,SIGTERM);
-    
-
+    kill(H3,SIGTERM);
+    kill(H1,SIGTERM);
+    kill(H2,SIGTERM);
+    kill(H4,SIGTERM);
+    printf("FInalizo como padre");
+    kill(getpid(),SIGKILL);
 }
 void alarmHandler(){
     manejadoraA.sa_handler = &funcionAlarma;
@@ -56,14 +55,15 @@ void manejadoraTestigo(){
     manejadoraT.sa_flags = SA_RESTART;
     sigaction(SIGUSR1,&manejadoraT,NULL);
 }
-void terminar(){
-    //SI tengo un hijo lo mato
-    //aqui tienen que entrar H2 y H3
+void terminar() {
     
-    //kill(temp,SIGINT);
-  
-    
-
+    if(N3 > 0){
+        kill(N3,SIGINT);
+    }else if (N2 > 0)
+    {
+        kill(N2,SIGINT);
+    }
+    kill(getpid(), SIGKILL);
 }
 void fterminateHandler(){
     terminateHandler.sa_handler = &terminar;
@@ -84,56 +84,49 @@ int main() {
     (H1 = fork()) && (H4 = fork())&& (H2 = fork()) && (H3 = fork()) ; // Creates 4 children
 
    // sleep(10);
-    if (H1 == 0) { //si le importa
+    if (H1 == 0) { 
         /* Child H1 code goes here */
         pidDisparo = getppid();
         while(1){
             sigsuspend(&maskTestigo);
-          //  kill(pidDisparo,SIGUSR1);
 
         }
     }else if(H4 == 0){ //se la pela
         pidDisparo = getppid();
         while(1){
-           // printf("PID disparo: %d\n",pidDisparo);
           sigsuspend(&maskTestigo);
-            //kill(pidDisparo,SIGUSR1);
         }
     }
      else if (H2 == 0) {
         N2 = fork();
-        if (N2 == 0) { // si le importa
+        if (N2 == 0) { 
             /* Grandchild N2 code goes here */
             pidDisparo = H1;
             while(1){
                  sigsuspend(&maskTestigo);
-              //  kill(pidDisparo,SIGUSR1);
 
             }
-        } else { //si le importa
+        } else if(N2 > 0){ 
             /* Child H2 code goes here */
             pidDisparo = N2; 
             while(1){
                 sigsuspend(&maskTestigo);
-                //kill(pidDisparo,SIGUSR1);
 
             }
+        }else if(N2 == -1){
+            perror("Error en N2");
+            return 1;
         }
     } else if(H3 == 0){
         N3 = fork();
-        if(N3 == 0){ //se la pela
+        if(N3 == 0){
             /* Grandchild N3 code goes here */
             pidDisparo = H4;
             while(1){
-                //printf("Pid H4 %d",H4);
-               // sleep(10);
-               // printf("PID disparo: %d\n",pidDisparo);
               sigsuspend(&maskTestigo);
-                //kill(pidDisparo,SIGUSR1);
             }
         }
-        if(N3 > 0){ //se la pela
-         // se la pela
+        else if(N3 > 0){
             /* Child H3 code goes here */
             pidDisparo = N3;
 
@@ -143,9 +136,13 @@ int main() {
 
             }
         }
+        else  if(N3 == -1){
+            perror("Error en N3");
+            return 1;
+        }
     }
     else if(H1 == -1 || H2 == -1 || H3 == -1 || H4 == -1){
-        perror("fork");
+        perror("Error en algun H");
         return 1;
     }
     else {
@@ -156,16 +153,12 @@ int main() {
         while(1){
             
             if(contador%2==0){ //si es par
-               // printf("voy para H2\n");
-               // sleep(5);
                 pidDisparo = H3;
+                printf("VUELTA: %d\n",contador/2);
             }else{
-               // printf("voy para H3\n");
-               // sleep(5);
                 pidDisparo = H2;
             }
-            printf("VUELTA: %d\n",contador/2);
-           // printf("H1 %d H2 %d H3 %d H4 %dN2 %d N3 %d\n",H1,H2,H3,H4,N2,N3);
+            
             sigsuspend(&maskTestigo);
             contador++;
         }
